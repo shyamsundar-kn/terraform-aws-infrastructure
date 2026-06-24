b58e59bcf3defb7e0cf15407195b567ec960faca
shyamsundar-kn <shyam.sundarkn01@gmail.com>
2026-06-24 12:30:56 +0530

    Create mian.tf

---
+++ modules/vpc/mian.tf
@@ -0,0 +1,11 @@
+aws_vpc
+aws_subnet (public)
+aws_subnet (private1)
+aws_subnet (private2)
+aws_internet_gateway
+aws_route_table (public)
+aws_route_table_association
+aws_eip
+aws_nat_gateway
+aws_route_table (private)
+aws_route_table_association
